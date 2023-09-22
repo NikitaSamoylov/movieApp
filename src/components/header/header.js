a22669cc69e17ac1562e54e9c323a23fd1a133fe
Nikita <nikita@MacBook.local>
2023-09-22 19:25:09 +0500

    feat: add search fetch API

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,9 +4,15 @@ import '../../assets/img/search-icon.png';
 import '../../assets/img/favorites.png';
 
 export class Header extends DivComponent {
-    constructor(appState) {
+    constructor(appState, state) {
         super();
         this.appState = appState;
+        this.state = state;
+    }
+
+    search(state) {
+        const value = this.el.querySelector('input').value;
+        this.state.searchQuery = value;
     }
 
     render() {
@@ -15,20 +21,21 @@ export class Header extends DivComponent {
         this.el.innerHTML = `
             <img class="header__logo" src="/assets/logo.png" alt="Movie">
             <div class="header__info header-info">
-                <div class="header-info__favorites">
-                    <img class="favorites-like" src="/assets/favorites.png">
-                    избранное 
-                      <span class="favorites-counter">${this.appState.favorites.length}
-                      </span>
-                </div>
-                <div class="header-info__search header-search">
-                    <input class="header-search__input" placeholder="я ищу фильм ...">
-                    <button class="header-search__button">
-                        <img class="header-search__img" src="/assets/search-icon.png" alt="искать">
-                    </button>
-                </div>
+            <div class="header-info__favorites">
+                <img class="favorites-like" src="/assets/favorites.png">
+                избранное 
+                  <span class="favorites-counter">${this.appState.favorites.length}
+                  </span>
             </div>
+            <div class="header-info__search header-search">
+                <input class="header-search__input" placeholder="я ищу фильм ..." value="${this.state.searchQuery ? this.state.searchQuery : ""}">
+                <button class="header-search__button">
+                    <img class="header-search__img" src="/assets/search-icon.png" alt="искать">
+                </button>
+            </div>
+        </div>
         `;
+        this.el.querySelector('button').addEventListener('click', this.search.bind(this))
         return this.el;
     }
 }
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,3 +1,4 @@
+import onChange from "on-change";
 import { AbstractView } from "../../common/view";
 import { Header } from "../../components/header/header";
 
@@ -5,6 +6,8 @@ export class MainView extends AbstractView {
     constructor(appState) {
         super();
         this.appState = appState;
+        this.appState = onChange(this.appState, this.appStateHook.bind(this));
+        this.state = onChange(this.state, this.stateHook.bind(this));
         this.setTitle('Поиск фильма');
     }
 
@@ -15,11 +18,41 @@ export class MainView extends AbstractView {
         offset: 0,
     }
 
+    appStateHook(path) {
+        console.log(path)
+        if (path === 'favorites') {
+            console.log(path)
+        }
+    }
+    async stateHook(path) {
+        if (path === 'searchQuery') {
+            this.state.loading = true;
+            const data = await this.loadFilms(this.state.searchQuery);
+            this.state.loading = true;
+            console.log(data);
+            this.state.list = data.docs;
+        }
+    }
+    
+    async loadFilms(name) {
+        const res = await fetch (`https://api.kinopoisk.dev/v1.2/movie/search?page=1&limit=10&query=${name}`, {
+            headers: {
+              'X-API-KEY': 'K8GMKAA-47P4S4H-MWPHWW0-K7WNG37'
+            }
+        })
+        return res.json();
+    }
+
     render() {
-        const main = document.createElement('div');
-        main.classList.add('main');
-        main.append(new Header(this.appState).render());
-        this.app.append(main);
+        // const main = document.createElement('div');
+        // main.classList.add('main');
+        // main.append(new Header(this.appState, this.state).render());
+        // this.app.append(main);
+        this.renderHeader()
+    }
+    renderHeader() {
+        const header = new Header(this.appState, this.state).render();
+        this.app.prepend(header);
     }
 
 }
